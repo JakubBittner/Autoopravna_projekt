21aafa7b4c0220ad6426de9b7b02a8aaf8029eb0
Jakub Bittner <D22504@oaopava.cz>
2025-04-17 17:01:36 +0200

    Změna reklam png plus přidání efektu v js a lehká uprava css

--- hlavni/static/js/script.js
+++ hlavni/static/js/script.js
@@ -20,3 +20,54 @@ const showSection = () => {
 
 window.addEventListener('scroll', showSection);
 window.addEventListener('load', showSection);
+
+const leveReklamy = [
+  {
+    src: "/static/img/michelin.png",
+    url: "https://www.michelin.cz"
+  },
+  {
+    src: "/static/img/toplac.png",
+    url: "https://eshop.toplac.cz"
+  }
+];
+
+const praveReklamy = [
+  {
+    src: "/static/img/castrol.png",
+    url: "https://www.castrol.com"
+  },
+  {
+    src: "/static/img/azpneu.png",
+    url: "https://www.az-pneu.cz"
+  }
+];
+
+let aktualniIndex = 0;
+
+setInterval(() => {
+  aktualniIndex = (aktualniIndex + 1) % leveReklamy.length;
+
+  const levaImg = document.getElementById("reklama-leva");
+  const levaOdkaz = document.getElementById("reklama-leva-odkaz");
+  const pravaImg = document.getElementById("reklama-prava");
+  const pravaOdkaz = document.getElementById("reklama-prava-odkaz");
+
+  // Fade out
+  levaImg.classList.add("fade-out");
+  pravaImg.classList.add("fade-out");
+
+  setTimeout(() => {
+    // Změna obrázků a odkazů
+    levaImg.src = leveReklamy[aktualniIndex].src;
+    levaOdkaz.href = leveReklamy[aktualniIndex].url;
+
+    pravaImg.src = praveReklamy[aktualniIndex].src;
+    pravaOdkaz.href = praveReklamy[aktualniIndex].url;
+
+    // Fade in
+    levaImg.classList.remove("fade-out");
+    pravaImg.classList.remove("fade-out");
+  }, 500); // Musí odpovídat transition času v CSS
+
+}, 30000); // Každých 30 sekund
